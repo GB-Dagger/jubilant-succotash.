6c651dbf567a8815643a7374b7c682cc70fa6543
Gabe Bryk <gbryk11@gmail.com>
2016-07-24 06:17:01 -0500

    solve triangle path

--- Euler/src/main/MaximumPath.java
+++ Euler/src/main/MaximumPath.java
@@ -1,25 +1,61 @@
 package main;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class MaximumPath {
-	public static int[][] triangle;
-	public static void main(String[] args) throws IOException {
-		File file = new File("smtrinum.txt");
-		FileReader fr = new FileReader(file);
-		BufferedReader reader = new BufferedReader(fr);
-		while ((reader.readLine()) != null) {
-			
+
+	
+	private static void findMax(int[][] triangle, int depth) {
+		int[] previous = null;
+		for(int i = 1; i < depth; i++) {
+			int[] last = triangle[depth - i];
+			previous = calculateRowMaximal(triangle[(depth - i) - 1], last);
+			printValues(previous);
 		}
-		reader.close();
-		
-		
+		System.out.println("result = " + previous[0]);
 	}
-	public static int[] getnums(String line){
-		return null;
-		
+	
+	private static int[] calculateRowMaximal(int[] previous, int[] last) {
+		for (int i = 0; i < previous.length; i++) {
+			previous[i] = previous[i] + (Math.max(last[i], last[i + 1]));
+		}
+		return previous;
 	}
-}	
+	
+	private static void printValues(int[] values) {
+		System.out.println();
+		for (int i : values) {
+			System.out.print("\\t" + i);
+		}
+	}
+	
+	private static int[][] getTriangle(String fileName, int depth) throws Exception {
+		int[][] triangle = new int[depth][];
+		FileReader fr = new FileReader(fileName);
+		BufferedReader br = new BufferedReader(fr);
+
+		String s;
+		int i = 0;
+		while ((s = br.readLine()) != null) {
+			triangle[i] = new int[i + 1];
+			int j = 0;
+			Scanner tokens = new Scanner(s);
+			while (tokens.hasNext()) {
+				int value = tokens.nextInt();
+				triangle[i][j++] = value;
+			}
+			i++;
+		}
+
+		return triangle;
+	}
+
+	public static void main(String args[]) throws Throwable {
+		String fileName = "smtrinum.txt";
+		int depth = 15;
+		int[][] triangle = getTriangle(fileName, depth);
+		findMax(triangle, depth);
+	}
+}

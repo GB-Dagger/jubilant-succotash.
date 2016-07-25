dd81a558417c2b400dce0db7ccd30fdc3194ee06
Gabe Bryk <gbryk11@gmail.com>
2016-07-24 22:54:38 -0500

    started on sunday code

--- Euler/src/main/CountingSundays.java
+++ Euler/src/main/CountingSundays.java
@@ -3,7 +3,16 @@ package main;
 public class CountingSundays {
 
 	public static void main(String[] args) {
-		
+		int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int[] leapyear = {31,29,31,30,31,30,31,31,30,31,30,31};
+		for(int year=1900;year<2000;year++){
+			if(year % 400 == 0){
+				for(int day=0;day<365;day++){
+					
+				}
+			}
+		}
 	}
+	public static 
 
 }

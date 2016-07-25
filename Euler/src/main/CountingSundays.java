5a72812c232bb712f416b172518c9dabec55f538
Gabe Bryk <gbryk11@gmail.com>
2016-07-25 10:55:43 -0500

    basic outline for counting sundays

--- Euler/src/main/CountingSundays.java
+++ Euler/src/main/CountingSundays.java
@@ -3,16 +3,26 @@ package main;
 public class CountingSundays {
 
 	public static void main(String[] args) {
-		int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
-		int[] leapyear = {31,29,31,30,31,30,31,31,30,31,30,31};
-		for(int year=1900;year<2000;year++){
+		for(int year=1901;year<2000;year++){
 			if(year % 400 == 0){
-				for(int day=0;day<365;day++){
-					
+				for(int month=0;month<12;month++){
+					if(ifLeapYear(year)){
+						
+					}
 				}
 			}
 		}
 	}
-	public static 
-
+	public static boolean ifLeapYear(int year){
+		if((year % 4 == 0) && ((year % 100 != 0)||(year % 400 == 0))){
+			
+		}
+		return false;
+	}
+	public int getDays(int month,boolean leap){
+		int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int[] leapyear = {31,29,31,30,31,30,31,31,30,31,30,31};
+		if(leap) return months[month+1];
+		else return leapyear[month+1];
+	}
 }

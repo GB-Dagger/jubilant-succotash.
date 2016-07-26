c0242974fecca1a94187dfc7263decaaa9d5a413
Gabe Bryk <gbryk11@gmail.com>
2016-07-26 18:23:26 -0500

    started on amicable num

--- Euler/src/main/AmicableNumbers.java
+++ Euler/src/main/AmicableNumbers.java
@@ -2,9 +2,17 @@ package main;
 
 public class AmicableNumbers {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		int[] amicableNums;
+		for(int i=0;i<10000;i++){
+			
+		}
 	}
-
+	public static int getDivisorSum(int num){
+		int sum = 0;
+		for(int i=1;i< num;i++){
+			if(num % i == 0) sum += i;
+		}
+		return sum;		
+	}
 }

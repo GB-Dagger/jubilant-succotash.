648e02a0d0545b7b669fc5f8229cf555523c2dab
Gabe Bryk <gbryk11@gmail.com>
2016-07-25 20:54:39 -0500

    almost done with sundays

--- Euler/src/main/CountingSundays.java
+++ Euler/src/main/CountingSundays.java
@@ -1,28 +1,45 @@
 package main;
 
-public class CountingSundays {
+import java.util.Arrays;
 
+public class CountingSundays {
+	
 	public static void main(String[] args) {
+		int day = 6;
+		int sundays = 0;
 		for(int year=1901;year<2000;year++){
-			if(year % 400 == 0){
-				for(int month=0;month<12;month++){
-					if(ifLeapYear(year)){
-						
-					}
+			while(true){
+				System.out.println("Year: " + year + " Day: " + day);
+				day+=7;
+				if(checkSunday(day, ifLeapYear(year))) sundays++;
+				
+				if(day>=365 && !ifLeapYear(year)){
+					day = day-365+1;
+					break;
 				}
+				else if(day>=366 && ifLeapYear(year)){
+					day = day-366+1;
+					break;
+				}
+				
 			}
 		}
+		System.out.println(sundays);
 	}
 	public static boolean ifLeapYear(int year){
-		if((year % 4 == 0) && ((year % 100 != 0)||(year % 400 == 0))){
-			
+		if(((year % 4 == 0))){
+			return true;	
 		}
-		return false;
+		else{
+			return false;
+		}
+		
 	}
-	public int getDays(int month,boolean leap){
-		int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
-		int[] leapyear = {31,29,31,30,31,30,31,31,30,31,30,31};
-		if(leap) return months[month+1];
-		else return leapyear[month+1];
+	public static boolean checkSunday(int day,boolean leap){
+		int[] monthslength = {1 ,32,60,91 ,121,152,182,213,244,274,205,335};
+		int[] leapyearlength={1 ,32,61,92 ,122,153,183,214,245,275,306,336};
+		if(leap && Arrays.asList(leapyearlength).contains(day)) return true;	
+		else if((!leap)&& Arrays.asList(monthslength).contains(day)) return true;	
+		else return false;
 	}
 }

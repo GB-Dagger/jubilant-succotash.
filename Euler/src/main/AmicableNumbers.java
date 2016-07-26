5fb5aad9d91f9a2ef9ccacef808ecb9ed2cdf37b
Gabe Bryk <gbryk11@gmail.com>
2016-07-26 18:23:43 -0500

    more amicable

--- Euler/src/main/AmicableNumbers.java
+++ Euler/src/main/AmicableNumbers.java
@@ -1,11 +1,15 @@
 package main;
 
+import java.util.Arrays;
+
 public class AmicableNumbers {
 
 	public static void main(String[] args) {		
-		int[] amicableNums;
+		int[] amicableNums = {0};
 		for(int i=0;i<10000;i++){
-			
+			if(!(Arrays.binarySearch(amicableNums, i ) >= 0)){
+				
+			}
 		}
 	}
 	public static int getDivisorSum(int num){

2be70ef1a197ce66361f3a56d4075d15840c1a5f
Gabe Bryk <gbryk11@gmail.com>
2016-07-26 22:40:57 -0500

    done with amicable started on nonaboundantsums

--- Euler/src/main/AmicableNumbers.java
+++ Euler/src/main/AmicableNumbers.java
@@ -1,16 +1,23 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AmicableNumbers {
 
 	public static void main(String[] args) {		
-		int[] amicableNums = {0};
-		for(int i=0;i<10000;i++){
-			if(!(Arrays.binarySearch(amicableNums, i ) >= 0)){
-				
+		List nums = new ArrayList<>();
+		for(int i=1;i<10000;i++){
+			if(!nums.contains(i)){
+				if((getDivisorSum(getDivisorSum(i)) == i) && (i != getDivisorSum(i))){
+					//System.out.println("added:" + i);
+					nums.add(i);
+					nums.add(getDivisorSum(i));
+				}
 			}
 		}
+		System.out.println("Size: " + nums.size() + " Sum: " + addList(nums));
 	}
 	public static int getDivisorSum(int num){
 		int sum = 0;
@@ -18,5 +25,14 @@ public class AmicableNumbers {
 			if(num % i == 0) sum += i;
 		}
 		return sum;		
+	}
+	public static int addList(List l){
+		int sum = 0;
+		//System.out.println(l.size());
+		for(int i = 0;i<l.size();i++){
+			sum += (int)l.get(i);
+			//System.out.println((l.get(i)));
+		}
+		return sum;
 	}
 }
---
+++ Euler/src/main/NonAbundantSums.java
@@ -0,0 +1,9 @@
+package main;
+
+public class NonAbundantSums {
+
+	public static void main(String[] args) {
+		
+	}
+
+}

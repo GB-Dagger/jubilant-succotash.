b1bc6e5267e579aced5579175bb6545e32947eff
Gabe Bryk <gbryk11@gmail.com>
2016-07-27 07:30:25 -0500

    started abundant

--- Euler/src/main/NonAbundantSums.java
+++ Euler/src/main/NonAbundantSums.java
@@ -5,5 +5,12 @@ public class NonAbundantSums {
 	public static void main(String[] args) {
 		
 	}
+	public static int getDivisorSum(int num){
+		int sum = 0;
+		for(int i=1;i< num;i++){
+			if(num % i == 0) sum += i;
+		}
+		return sum;		
+	}
 
 }

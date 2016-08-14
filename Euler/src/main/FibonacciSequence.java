967c8e1a98d57ce8d9b81084da026378b4492e0e
Gabe Bryk <gbryk11@gmail.com>
2016-08-14 09:31:31 -0500

    start reciprocal

--- Euler/src/main/FibonacciSequence.java
+++ Euler/src/main/FibonacciSequence.java
@@ -1,10 +1,28 @@
 package main;
 
+import java.math.BigInteger;
+
 public class FibonacciSequence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		long start = System.currentTimeMillis();
+		System.out.println("Fibonacci sequence of 1000: " + fibonacci(1000));
+		System.out.println("Time: " + (System.currentTimeMillis()-start));
 	}
-
+	public static long fibonacci(int index){
+		BigInteger last = new BigInteger("13");
+		BigInteger current = new BigInteger("21");
+		long count;
+		for(count = 8;1000 > current.toString().length();count++){
+			BigInteger temp = new BigInteger(last.add(current).toString());
+			last = current;
+			current = temp;
+			//System.out.println(current);
+		}
+		return count;
+	}
+	public static void next(long i,long last){
+		i = i + last;
+		//return i;
+	}
 }
---
+++ Euler/src/main/ReciprocalCycles.java
@@ -0,0 +1,16 @@
+package main;
+
+public class ReciprocalCycles {
+	public static long longest = 0;
+	public static void main(String[] args) {
+		for(int d=0;d<1000;d++){
+			
+		}
+	}
+	public static long divide(int divisor){
+		
+		return divisor;
+		
+	}
+	
+}

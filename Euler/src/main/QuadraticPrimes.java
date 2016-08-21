0f2db5bf522521c7fc38250444cf4f2a9892e9c9
SutterButter4 <jsutter909@gmail.com>
2016-08-20 19:25:10 -0500

    fixing

--- Euler/src/main/QuadraticPrimes.java
+++ Euler/src/main/QuadraticPrimes.java
@@ -1,7 +1,40 @@
 package main;
 
+import java.util.ArrayList;
+
 public class QuadraticPrimes {
 	public static void main(String[] args) {
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		int aSave = -111111;
+		int bSave = -111111;
+		int consecutivePrimeCount = 0;
+		for(int a = -999; a<=999; a++){
+			System.out.println(" A:" + a);
+			for(int b = -999; b<=999; b++){
+				boolean continueBool = true;
+				for(int n = 0; continueBool; n++){
+					int num = n*n+a*n+b*n*n+a*n+b;
+					if(!isPrime(num)){
+						continueBool = false;
+						if(n-1 > consecutivePrimeCount){
+							aSave = a;
+							bSave = b;
+							consecutivePrimeCount = n-1;
+							System.out.println("New Number Reached!");
+						}
+					}
+				}
+			}
+		}
+		System.out.println("A:" + aSave + " B:" + bSave + " PrimeCount:" + consecutivePrimeCount);
 		
+	}
+	public static  boolean isPrime(int in){
+		for(int i = 1;  i <= in/2; i++){
+			if(in%i == 0){
+				return false;
+			}
+		}
+		return true;
 	}
 }
